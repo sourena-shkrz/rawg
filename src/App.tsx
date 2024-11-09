2bbe5dd78017d6b1125944d7ca8f4f3cc0aa0d60
Saeed Shokrzadeh -Home <s.shokrzadeh@gmail.com>
2024-11-09 14:07:04 +0330

    build Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-
-import { Button, ButtonGroup } from "@/components/ui/button";
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Button colorPalette='blue'>Button</Button>
+    <Grid
+      templateAreas={{
+        base : '"nav" "main"',
+        lg: '"nav nav" "aside main"',
+      }}
+    >
+      <GridItem area="nav" bg="red">
+        Nav
+      </GridItem>
+      
+      <GridItem area="aside" bg="blue" hideBelow='lg' >
+        aside
+      </GridItem>
+
+      <GridItem area="main" bg="gold">
+        main
+      </GridItem>
+    </Grid>
   );
 }
 
